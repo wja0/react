801c12807b91c06497d1e1022f597c5b262fc5ed
DESKTOP-SSTRNI0\HOON <lee040118@naver.com>
2021-01-18 13:24:14 +0900

    update submit_page 0118

--- eucalyptus/src/pages/index.js
+++ eucalyptus/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as problemset } from './problemset';
 export { default as problem } from './problem';
 export { default as NotFound } from './NotFound';
+export { default as submit } from './submit';
