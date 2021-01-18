172b20f737fb183ebafba2ec9e9cfa8adcbd1655
wja0 <dnwkdud99@gmail.com>
2021-01-18 14:47:25 +0900

    add status page

--- eucalyptus/src/pages/Pages.js
+++ eucalyptus/src/pages/Pages.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Switch , Route} from 'react-router-dom'
-import {NotFound, problem ,Home , problemset } from './index';
+import {NotFound, problem ,Home , problemset, status } from './index';
 
 const Pages = () => (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path='/problemset' component={problemset} />
         <Route path='/problem' component={problem} />
+        <Route path='/status' component={status} />
         <Route component={NotFound} />
     </Switch>
 );
