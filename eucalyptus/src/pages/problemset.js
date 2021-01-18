279c3fb62091106c6da9926bc83b06a3bde901bf
mjkim317 <mjkim317@naver.com>
2021-01-19 03:16:06 +0900

    create problemset.js

--- eucalyptus/src/pages/problemset.js
+++ eucalyptus/src/pages/problemset.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import ProblemList from "./Problemget";
 
 class problemset extends Component{
+    state = {
+        Problems: []
+    };
+    
+    getList = async () => {
+        const {
+            data:
+            { Problems }
+        } =  await axios.get('https://mjkim317.github.io/react/eucalyptus/src/pages/data.json');
+
+        console.log(Problems);
+        this.setState({ Problems });
+    }
+
+    componentDidMount() {
+        this.getList();
+    }
+
     render() {
-        return(
-            <div>
-                problems
-            </div>
+        const { Problems } = this.state;
+        return (
+            <section className = "container">
+                <div className = "pset">
+                    {Problems.map((prob,index) => (
+                        <ProblemList
+                        key={index}
+                        pnum={prob.Pnum}
+                        pname={prob.Pname}
+                        solved={prob.Solved}
+                        snum={prob.SNum}
+                        />
+                    ))}
+                </div>
+            </section>
         );
     }
 }
 
 export default problemset
+
