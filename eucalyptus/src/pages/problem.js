6555ee99e9e7400b1e23e1aca1dfc1427835467e
nature1216 <annekim1216@naver.com>
2021-01-19 04:23:37 +0900

    create ViewProblem.css

--- eucalyptus/src/pages/problem.js
+++ eucalyptus/src/pages/problem.js
@@ -4,32 +4,16 @@ import ViewProblem from "../components/ViewProblem";
 
 class Problem extends React.Component {
     state = {
-        /*
-        info:
-        {
-            "Pnum" :"1001" ,
-            "Pname": "A+B" ,
-            "Solved" : "T" ,
-            "Pcond" : ["0.5초","128MB"],
-            "Pdetail" : [
-                "두 개의 숫자 a, b를 입력받아 합을 출력하시오.",
-                "0 < a,b <100",
-                "첫째 줄에 a+b를 출력한다."
-            ],
-            "Pinout" : [
-                ["20 4"],
-                "24"
-            ]
-        }
-        */
+        info: []
     };
     
     getinfo = async () => {
         const {
-            Pnum, Pname, Solved, Pcond, Pdetail, Pinout
+            data:
+                {info}
         } = await axios.get("https://nature1216.github.io/react/eucalyptus/src/pages/data.json");
-        console.log({Pnum, Pname, Solved, Pcond, Pdetail, Pinout});
-        this.setState( {Pnum, Pname, Solved, Pcond, Pdetail, Pinout} );
+        console.log({info});
+        this.setState( {info} );
     };
     componentDidMount() {
         this.getinfo();
@@ -38,7 +22,18 @@ class Problem extends React.Component {
         const { info } = this.state;
         return (
             <div className="info">
-                {info.Pnum}
+                {info.map(info => (
+                    <ViewProblem
+                        key={info.Pnum}
+                        Pnum={info.Pnum}
+                        Pname={info.Pname}
+                        Solved={info.Solved}
+                        Pcond={info.Pcond}
+                        Pdetail={info.Pdetail}
+                        Pinout={info.Pinout}
+                    />
+                ))}
+                
             </div>
         )
     }
