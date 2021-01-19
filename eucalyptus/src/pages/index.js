b659e2d4216def4074a1e19d48fdd6d5b1ed3774
DESKTOP-SSTRNI0\HOON <lee040118@naver.com>
2021-01-19 11:31:20 +0900

    update_문제 내용&리스트 0119

--- eucalyptus/src/pages/index.js
+++ eucalyptus/src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from './Home';
 export { default as problemset } from './problemset';
-export { default as problem } from './Problem';
+export { default as problem } from './problem';
 export { default as NotFound } from './NotFound';
 export { default as status } from './status';
 export { default as submit } from './submit';
