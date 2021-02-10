feaaa54b81e5a2f899a35a9ed4c9f93fbd1e8529
wja0 <dnwkdud99@gmail.com>
2021-02-10 15:28:53 +0900

    modify LoginForm

--- eucalyptus/src/layout/LoginForm.js
+++ eucalyptus/src/layout/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Login.css';
 import { Login } from '../pages';
+import sha256 from 'crypto-js/sha256'; // 해싱 -> npm install crypto-js
 
 class LoginForm extends Component {
     state = {
@@ -9,13 +10,31 @@ class LoginForm extends Component {
     }
     handleChange= (e) => {
         this.setState({
-            [e.target.name] : e.target.value
+            [e.target.name] : e.target.value,
             // [e.target.name]: 설정한 태그의 name값을 event 객체를 통해 가져와서 사용함
-        });
+          });
     }
-    handleSumit = (e) => {
-        e.preventDefault();                 //페이지 리로딩 방지(상태 잃어버리는 것 방지)
+    handleSumit = async (e) => {
+        e.preventDefault();                 //페이지 리로딩 방지(상태 잃어버리는 것 방지)        
+        var hash = String(sha256(this.state.password)); // 비밀번호 hash 값으로 인코딩
+        this.state.password = hash;         // 인코딩 값으로 password 값 업데이트
+        this.setState(this.state);          //상태값 업데이트
         this.props.onCreate(this.state);    //상태값 onCreate 통해 부모에게 전달
+        // console.log로 값 확인
+        
+        // 서버 전송 코드. 정확하지 않음ㅠ
+        // const {
+        //   data: 
+        //   { id, password },
+        // } = await axios({
+        //   method: "post",
+        //   url: "http://210.117.181.118:4848/spring/",
+        //   data: {
+        //     ID : this.state.id,
+        //     Pwd : this.state.password,
+        //   },
+        // }).then();
+
         this.setState({                     //상태초기화
             id: '',
             password: ''
