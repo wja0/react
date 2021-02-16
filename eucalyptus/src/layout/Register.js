087c91400ecc90cda5ab2ff43e58635c069856a1
mjkim317 <mjkim317@naver.com>
2021-02-16 22:00:02 +0900

    modify Register

--- eucalyptus/src/layout/Register.js
+++ eucalyptus/src/layout/Register.js
@@ -3,12 +3,28 @@ import RegisterForm from './RegisterForm';
 
 
 class Register extends Component {
+    state= {
+        register_info: []
+    }
+    handleCreate = (data) => {
+        console.log(this.number);
+        console.log(data);
+        const { register_info } = this.state;
+
+        this.setState({
+            nickname: data.nickname,
+            id: data.id,
+            password: data.password,
+            check_password: data.check_password,
+        })
+    }
+
     render() {
         return (
           <div>
-            <RegisterForm/>
+            <RegisterForm onCreate={this.handleCreate}/>
           </div>
         );
-      }
-  }
+    }
+}
 export default Register;
