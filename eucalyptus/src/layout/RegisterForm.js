86cd8fc96ec15acb1e789f4a2adbb3c665b9ff1a
unknown <luck2901@naver.com>
2021-03-08 13:25:51 +0900

    3/8 RegisterForm

--- eucalyptus/src/layout/RegisterForm.js
+++ eucalyptus/src/layout/RegisterForm.js
@@ -72,7 +72,7 @@ class RegisterForm extends Component{
     handleClick = () =>{
         const data = axios({
             method:"post",
-            url:"https://luck2901.github.io/react/",
+            url:"https://luck2901.github.io/react",
             data:{
                 ID : this.state.id,
             },
