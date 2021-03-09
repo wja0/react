7fc5db1a7c37670821a5e6a4641661004fc8cb8a
Hoon <lee040118@naver.com>
2021-03-09 20:23:18 +0900

    Update 회원가입페이지

--- eucalyptus/src/layout/RegisterForm.js
+++ eucalyptus/src/layout/RegisterForm.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component, useEffect, useRef } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import sha256 from 'crypto-js/sha256';
+import axios from "axios";
 import './Register.css';
 
 class RegisterForm extends Component{
-    state = {
-        nickname:'',
-        id:'',
-        password: '',
-        check_password: '',
-        correct:''
-    }
-
-    doneAlert= (e) => {
-        alert("회원가입 완료!");
+    constructor(props) {
+        super(props);
+        this.handleSumit = this.handleSumit.bind(this);
+        this.state = {
+            name: '',
+            id: '',
+            password: '',
+            check_password: '',
+            correct: '',
+            idcheck : false,
+            result : '',
+        };
+      }
+    passwordCheck =() =>{
+        const {password, check_password} = this.state();
+        if(password.length<1 || check_password<1) return false;
+        else if(password ===check_password) return true;
+        else return false;
     }
 
     handleChange= (e) => {
@@ -25,17 +35,56 @@ class RegisterForm extends Component{
         setTimeout(this.handleCheck, 100);       
     }
     handleSumit = (e) => {
-        e.preventDefault();                 //페이지 리로딩 방지(상태 잃어버리는 것 방지)
-        this.props.onCreate(this.state);    //상태값 onCreate 통해 부모에게 전달
-        this.setState({                     //상태초기화
-            nickname: '',
-            id: '',
-            password: '',
-            check_password: '',
-            correct: ''
-        })
+        e.preventDefault();
+            if(this.state.name==0)
+            {
+                window.alert('이름을 입력하세요')
+            }
+            else if(this.state.id==0)
+            {
+                window.alert('아이디를 입력하세요')
+            }
+            else if(this.state.password>=1&&this.state.check_password>=1&&this.state.password!==this.state.check_password){
+                window.alert("비밀번호를 일치시켜주세요.");
+            }
+            else if(this.state.password==0 || this.state.password==0)
+            {
+                window.alert('비밀번호를 입력하세요')
+            }
+            else if(this.state.idcheck == false)
+            {
+                window.alert('아이디 중복 확인 검사필요')
+            }
+            else {
+                var hash = String(sha256(this.state.password)); 
+                this.state.password = hash;     
+                this.state.check_password = hash;    
+                this.setState(this.state);          //상태값 업데이트
+                this.props.onCreate(this.state);    //상태값 onCreate 통해 부모에게 전달
+                const data  =  axios({
+                    method: "post",
+                    url: "http://210.117.181.118:4848/spring/register",
+                    data: {
+                        Name : this.state.name,
+                        ID : this.state.id,
+                        Pwd : this.state.password,
+                    },
+                    }).then(function (result) {
+                        console.log(result.data)
+                        // this.state.result = result.data
+                    });
+                    window.alert('회원가입 완료!')
+                    this.props.history.push({
+                        pathname: `/register/done`,
+                        state: {
+                        result: this.result,
+                        id : this.id
+                        },
+                    });
+                    
+            }
+        
     }
-
     handleCheck = () => {
         const{ password, check_password } = this.state;
         if(password.length < 1 || check_password.length < 1){
@@ -48,6 +97,25 @@ class RegisterForm extends Component{
             this.setState({correct:'비밀번호 불일치'});
         }
     }
+    handleClick = () =>{ //아이디 중복확인 이벤트
+        const data = axios({
+            method:"post", //get으로 진행했을 때 isunique:"true" 식으로 반환.
+            url:"http://210.117.181.118:4848/spring/isunique",
+            data:{
+                ID : this.state.id,
+            },
+        }).then(function(result){
+            console.log(result.data);
+            if(result.data.isUnique == "True"){
+                window.alert('사용가능한 아이디입니다')
+                this.setState({idcheck : true})
+            }
+            else{
+                window.alert('사용할 수 없는 아이디입니다')
+            }    
+        });
+        this.setState({idcheck : true})
+    }
 
     render() {
       return (
@@ -56,10 +124,10 @@ class RegisterForm extends Component{
           <h1>회원가입</h1>
           
           <div className="nickname">
-                <input placeholder="enter your nickname"
-                       value={this.setState.nickname}
+                <input placeholder="enter your name"
+                       value={this.state.name}
                        onChange={this.handleChange}
-                       name="nickname">
+                       name="name">
                 </input>
           </div>
           <div className="makeID">
@@ -68,7 +136,9 @@ class RegisterForm extends Component{
                        onChange={this.handleChange}
                        name="id">
                 </input>
-                <button type="button">아이디 중복 확인</button>
+                <button type="button"
+                        onClick={this.handleClick}>
+                    아이디 중복 확인</button>
           </div>
 
           <div className="makePassword">
@@ -89,9 +159,7 @@ class RegisterForm extends Component{
           <div className="correct"> {this.state.correct} </div>
 
           <div className = 'reg'>
-                <Link to ="/Register/Done">
-                    <button className="join" >가입하기</button>
-                </Link>
+              <button className="join" >가입하기</button>
             </div>
         </div>
         </form>
@@ -99,4 +167,4 @@ class RegisterForm extends Component{
     }
 }
 
-export default RegisterForm;
+export default withRouter(RegisterForm); 
