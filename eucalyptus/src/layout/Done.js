25a361449f25820c62add6b9de114d95082bd5c9
mjkim317 <mjkim317@naver.com>
2021-03-09 18:01:48 +0900

    create Register_Done page

---
+++ eucalyptus/src/layout/Done.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Done extends Component {
+
+    render() {
+      return (
+        <div>
+            <h1>회원가입이 완료되었습니다!</h1>
+
+            <div className = 'done'>
+                <Link to ="/Login">
+                    <button className="doneButton">로그인으로 이동</button>
+                </Link>
+            </div>
+        </div>
+      );
+    }
+  }
+
+export default Done;
