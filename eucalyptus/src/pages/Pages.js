7cfae45c490f44126fda5e0d35ee288194fc1c4f
lalala5772 <65425885+lalala5772@users.noreply.github.com>
2021-03-13 01:12:26 +0900

    Update Pages.js

--- eucalyptus/src/pages/Pages.js
+++ eucalyptus/src/pages/Pages.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {Switch , Route} from 'react-router-dom';
-import {status, submit ,NotFound, problem ,Home , problemset, Tmp, Login } from './index';
+import Register from '../layout/Register';
+import Done from '../layout/Done'
+import {status, submit ,NotFound, problem ,Home , problemset, Tmp, Login, User, source } from './index';
 
 const Pages = () => (
     <Switch>
-        <Route exact path="/" render={(props) => <Home bh={false}/>} />
+        {/* <Route exact path="/" render={(props) => <Home bh={false}/>} /> */}
+        <Route exact path="/" component={Home}/>
         <Route path='/problemset' component={problemset} />
         <Route path='/problem/:id' component={problem} />
         <Route path='/status/:id' component={status} />
         <Route path='/submit/:id' component={submit} />
-        {/* <Route path='/submit/:id' render={(props) => <submit />} /> */}
+        {/* <Route path='/submit/:id' render={(props) => <submit />} /> */}  
+        <Route path='/user/:id' component={User} />
         <Route path='/Tmp' component={Tmp} />
         <Route path='/Login' component={Login} />
+        <Route exact path='/Register' component={Register}/>
+        <Route path='/Register/Done' component={Done}/>
+        <Route path='/source:subNum' component={source} />
         <Route component={NotFound} />
     </Switch>
 );
