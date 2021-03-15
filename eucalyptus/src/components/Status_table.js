18d3eda344f3373595347a9c9116e47b6a7da5ab
wja0 <dnwkdud99@gmail.com>
2021-03-15 22:55:23 +0900

    modify status.js

--- eucalyptus/src/components/Status_table.js
+++ eucalyptus/src/components/Status_table.js
@@ -3,24 +3,37 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Status_table.css";
 
-function Status_table({ SubNum, Result }) {
+function Status_table({ statusList }) {
   return (
     <tbody>
       <tr className="list_status">
-        <td  style={{ width: "20%" }}>
+        <td style={{ width: "8%" }} className="제출 번호">
+          {statusList.SubNum}</td>
+        <td style={{ width: "10%" }} className="문제 번호">
           <Link to={{
-            pathname: `/problem`,
-          }}>{SubNum}</Link></td>
-        <td style={{ width: "80%" }}>
-          {Result}</td>
+            pathname: `/problem/${statusList.Pnum}`,
+          }}>{statusList.Pnum}</Link></td>
+          <td style={{ width: "10%" }} className="아이디">
+          <Link to={{
+            pathname: `/user/${statusList.ID}`,
+          }}>{statusList.ID}</Link></td>
+        <td style={{ width: "40%" }} className="결과">
+          {statusList.Permission?
+            <Link to={{pathname: `/source/${statusList.subNum}`,}}>{statusList.Result}</Link> 
+            : alert('권한 없음')}   
+        </td>
+        <td style={{ width: "10%" }} className="언어">
+          {statusList.LangType}</td>
+        <td style={{ width: "10%" }} className="코드 길이">
+          {statusList.CodeSize}</td>
       </tr>
     </tbody>
   );
-}
+}//Permission true일때만 결과 출력. false이면 '권한 없음' 경고창 
+//(데이터가 없어서 false일 때는 확인 못해봄)
 
 Status_table.propTypes = {
-  SubNum: PropTypes.string.isRequired,
-  Result: PropTypes.string.isRequired
+  
 };
 
 export default Status_table;
--- eucalyptus/src/pages/status.js
+++ eucalyptus/src/pages/status.js
@@ -9,46 +9,55 @@ class status extends Component{
     constructor(props){
       super(props)
       this.state = {
-        result : props.location.state.result,
-        Pnum : props.match.params.id
+        statusList: []
       };
+    // console.log(props);
     }
 
-      // getProblems = async () => {
-      //   const {
-      //     data:
-      //     {Problems}
-      //   } = await axios.get('https://coala-oj.github.io/react/eucalyptus/src/pages/채점결과.json');
-      //   console.log({Problems});
-      //   this.setState({ Problems });
-      // };
-      // componentDidMount() {
-      //   // this.getProblems();
-      //   // console.log(this.props)
-      //   // const {result} = this.props
-      //   this.setState({ result : this.props.location });
-      // }
+    getStatus = async () => {
+      const {
+          data: { Status }
+      } = await axios ({
+          method : 'post',
+          // url : 'http://210.117.181.118:4848/spring/scoringboard',
+          url : 'http://218.151.66.186:8080/spring/scoringboard',
+          // url : 'https://wja0.github.io/react/eucalyptus/src/scoringboard.json',
+          data : {
+              ID : 'j'
+          },
+      }).then()
+      this.setState( {
+          statusList : Status
+      } );
+      console.log(this.state)
+    };
+
+    componentDidMount() {
+      this.getStatus();
+    }
 
       render() {
-        const { result, Pnum } = this.state;
-        return (
-
+        const { statusList } = this.state;
+        return (      
           <div className="Problems">
-            <Subnavigation Pnum={Pnum} />
+            <Subnavigation Pnum={statusList.Pnum} />
             <div className="Problems_container">
               <table className="status__table" id="problem">
                 <thead>
                   <tr>
-                    <th style={{width: "20%"}}>제출 번호</th>
-                    <th style={{width: "80%"}}>채점 결과</th>
+                    <th style={{width: "8%"}}>제출 번호</th>
+                    <th style={{width: "10%"}}>문제 번호</th>
+                    <th style={{width: "10%"}}>아이디</th>
+                    <th style={{width: "40%"}}>결과</th>
+                    <th style={{width: "10%"}}>언어</th>
+                    <th style={{width: "10%"}}>코드 길이</th>
                   </tr>
                 </thead>
-                
-                {result.map(result => (
+
+                {statusList.map(statusList => (
                   <Status_table
-                    key={result.SubNum}
-                    SubNum={result.SubNum}
-                    Result={result.Result}
+                    key={statusList.id}
+                    statusList={statusList}
                   />
                 ))}
               </table>
